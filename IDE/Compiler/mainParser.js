3f9c3ce8a044f67e3684bb8c48e151c25fc25150
Teuber <michael@teuber.dk>
2019-05-05 23:51:07 +0200

    Comments & cleaning

--- IDE/Compiler/mainParser.js
+++ IDE/Compiler/mainParser.js
@@ -1,5 +1,5 @@
         //Splits on newline into array
-        //also removes white space and changes everything to lower case
+        //Removes white space and changes everything to lower case
         //code = string
         //return = array with trimmed strings split on \n
         function codeReader(code){
@@ -10,12 +10,13 @@
             return codeLines;
         };
 
-        
+        //Searches for a specific variable
+        //name = name of variable to look up
+        //variables = list with lists of variables
+        //return = string with type of variable found or error if nothing was found
         function lookUpVar(name, variables){
-            //console.log(variables);
             for (stack in variables){
                 for (vars in stack){
-                    //console.log("lookup: " + variables[stack][vars]);
                     if (variables[stack][vars] && name === variables[stack][vars].name){
                         return variables[stack][vars].vartype;
                     }
@@ -24,12 +25,14 @@
             return {"type": "error", "errortype": "could not find var"};
         }
 
+        //Creates a string with c variable creations
+        //typeObj = json with information about variable to be created
+        //expression = expression part of variable declaration
+        //return = string with c variable declaration
         function varWriter(typeObj, expression){
-            //console.log(typeObj.vartype);
             switch(typeObj.vartype){
                 case "int":
                     const newLine = typeObj.vartype + " " + typeObj.name + " = " + expression + ";\n";
-                    //console.log("newLine: " + newLine); 
                     return newLine;
                     break;             
                 case "char":
@@ -43,9 +46,11 @@
             return {"error": "internal", "errortype": "varWriter - no match"}
         }
 
+        //Tests if a char is a part of a set of chars
+        //char = char to be tested
+        //set = set to be tested against
+        //return = true if char is a part of set otherwise false
         function testChar(char, set){
-            //console.log(char);
-            //console.log(set);
             for (chars in set){
                 if (char === set[chars]){
                     return true;
@@ -54,18 +59,16 @@
             return false;
         };
 
-        //maybe change to error?
+        //Verifies if name only contains legal chars
+        //nameStr = string with the potential new name that is to be testes
+        //return = {"type": "ok"} if things go well otherwise an error
         function verifyName(nameStr){
             const numbers = ["0","1","2","3","4","5","6","7","8","9"];
             const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
             if (nameStr.length > 0){
-                //console.log("result of test: " + testChar(nameStr.charAt(0)), letters);
                 if (testChar(nameStr.charAt(0), letters)){
                     for (let i = 1; i < nameStr.length; i++){
-                        //console.log(!testChar(nameStr.charAt(i), numbers));
-                        //console.log(!testChar(nameStr.charAt(i), letters));
                         if (!(testChar(nameStr.charAt(i), numbers) || testChar(nameStr.charAt(i), letters))){
-                            //console.log(nameStr.charAt(i));
                             return {"type": "error", "error": "name error wrong char"};
                         };
                     };
@@ -76,6 +79,9 @@
             return {"type": "error", "error": "name error too short"};;
         };
 
+        //Tests is a simpl expression results in an int result
+        //expr = expression to be tested
+        //return = {"type": "int"} if expr is an int otherwise error
         function intExpressionHandler(expr){
             const numbers = ["0","1","2","3","4","5","6","7","8","9"];
             const operators = ["+","-","/","*"];
@@ -116,45 +122,41 @@
             return {"type": "error", "error": "int epr too short"}
         };
 
-        //identifier??? verify type? typeChecker
+        //Chooses correct handler according to the expected type
+        //expr = expression to be passed to correct handler
+        //typeExpected = the expected type of expr
+        //return =  result of specific handler or error if no handler could be found
         function expressionHandler(expr, typeExpected){
-            //console.log("called exphandler, type expected: " + typeExpected);
             switch (typeExpected){
                 case "int":
                     return intExpressionHandler(expr);
                     break;
             };
             return {"type": "error", "error": "could not find handler for expression"}
-            //return "int";
         };
 
-        //get indent?
+        //verifies variables and pushes them onto stack
+        //newVar = json with information about a new variable
+        //line = line of simpl code with variable declaration
+        //variables = all current variables
+        //return = result of varWriter() or error
         function variableHandler(newVar, line, variables){
             //verify name
             const name = line.substring(newVar.vartype.length, newVar.operator);
-
             const verifiedName = verifyName(name);
-            //console.log("verify name");
-            //console.log(verifiedName);
             if (verifiedName.type === "error"){
                 return verifiedName;
-            };
-
+            };            
             if (lookUpVar(name, variables).type !== "error"){
                 return {"type": "error", "error": "var already created"};
             };
-
+            //verify expression
             const expression = line.substring(newVar.operator + 1, line.length);
             const expressionType = expressionHandler(expression, newVar.vartype);
-
             if (expressionType.type === "error"){
                 return expressionType;
-            }
-
-            if (lookUpVar(name, variables))
-            //console.log("name: " + name);
-            //console.log("expression: " + expression);
-            //console.log("expression type: " + expressionType);
+            };
+            //create new variable if no type error
             if (newVar.vartype === expressionType.type){
                 newVar = {...newVar, "name": name};
                 variables[0].push(newVar);
@@ -163,8 +165,10 @@
             return {"type": "error", "error": "Type error"}
         };
 
+        //Identifies contents of a line of simpl code
+        //line = string with a simpl code line
+        //return = json with information about type simpl code
         function lineIdentifier(line){
-            //console.log(line);
             const simplType = [{"id": "int"}, {"id": "void"}];
             const simplConditional = [{"id": "while"}, {"id": "for"}, {"id": "if"}]
             for (rule in simplType){
@@ -172,7 +176,6 @@
                     if (simplType[rule].id === "void"){
                         const endOfName = line.indexOf("(");
                         if (endOfName > 0){
-                            //return endOfName
                             return {"type": "function", "functiontype": simplType[rule].id};
                         }
                         return {"type": "error", "error": "void but no function"};
@@ -183,7 +186,6 @@
                     };
                     //test for function or variable declaration
                     for (let i = simplType[rule].id.length; i < line.length; i++){
-                        //return i as well!!
                         if (line.charAt(i) === "("){
                             return {"type": "function", "functiontype": simplType[rule].id, "operator": i};
                         }
@@ -203,11 +205,13 @@
 
             return {"type": "error", "error": "Keyword not found"};
         };
-
+        //controls parsing
+        //codeArray = array with lines of simpl code trimmed by codeReader()
+        //return = 
         function mainParser(codeArray){
+            //all current variables
             let variables = [[]];
             for (codeLine in codeArray){
-                //console.log(lineIdentifier(codeArray[codeLine]));
                 const statement = lineIdentifier(codeArray[codeLine]);
                 switch (statement.type){
                     case "error":
@@ -218,7 +222,6 @@
                     case "conditional":
                         break;
                     case "var":
-                        //console.log("end of line: ");
                         console.log(variableHandler(statement, codeArray[codeLine], variables));
                         break;
                     case "list":
