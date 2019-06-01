cc84e77c98cc0f292768b3c29bc88e80d9c782fb
Teuber <michael@teuber.dk>
2019-06-02 01:24:41 +0200

    Update stringParser.js

--- IDE/Compiler/stringParser.js
+++ IDE/Compiler/stringParser.js
@@ -1,3 +1,6 @@
+//Parses a Simpl string expression
+//expression = expression identified as being a string
+//return = {"type": "string"} if type of expression is string
 function stringExpressionParser(expression){
     const exprList = splitOnOperator(expression, stringOperators);
     let cantBeOperator = true;
@@ -40,64 +43,3 @@ function stringExpressionParser(expression){
     };
     return {"id": "error", "type": "error", "error": "missing string expression"};
 };
-/*
-function verifyStringExpr(expression){
-    const exprList = splitOnOperator(expression, stringOperators);
-    let cantBeOperator = true;
-    if(testChar(exprList[exprList.length - 1], stringOperators)){
-        return {"type": "error", "error": "unexpected operator - String ending in operator"}
-    };
-
-    for (let expr = 0; expr < exprList.length; expr++){
-        const nextChar = exprList[expr].charAt(0);
-        if (testChar(nextChar, stringOperators)){
-            if (cantBeOperator){
-                return {"type": "error", "error": "unexpected operator", "char": nextChar};
-            }
-            else{
-                cantBeOperator = true;
-            };
-        }
-        else if (nextChar === "\"" && exprList[expr].charAt(exprList[expr].length-1) === "\""){
-            cantBeOperator = false;
-        }
-        else if (testChar(nextChar, letters)){
-            funcOperator = exprList[expr].indexOf("(");
-            if (funcOperator > 0){
-                const func = functionLookup(exprList[expr].substring(0, funcOperator));
-                if (func.error){
-                    return func;
-                }
-                if (func.functiontype === "string"){
-                    const args = functionCallArgHandler(func, expression);
-                    if (args.error){
-                        return args;
-                    };
-                    funcEnd = expression.indexOf(")");
-                    if(expression.length > funcEnd + 1){
-                        return verifyStringExpr("0" + expression.substring(funcEnd + 1, expression.length));
-                    }
-                    return {"type": "string"};
-                }
-                else {
-                    return {"type": "error", "error": "type mismatch "+ func.name + " does not return a string"};
-                };
-            }
-            else {     
-                const varType = lookUpVar(exprList[expr]);
-                if (varType.error){
-                    return varType;
-                };
-                if (varType !== "string"){
-                    return {"type": "error", "error": "type mismatch "+ exprList[expr] + " does not return a string"};
-                };
-                if (varType === "string"){
-                    cantBeOperator = false;
-                };
-            };
-        }else{
-            return {"type": "error", "error": "String must start and end with \""};
-        };
-    };
-    return {"type": "string"};
-};*/
