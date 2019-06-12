8fcfbcce32d1b80af3664ec4c9db9ffb08619017
Teuber <michael@teuber.dk>
2019-06-12 16:03:22 +0200

    Small additions
    Fixed c code (changed "c{" and "}" to "C#" and "#")
    added temporary blinkLED file
    fixed function calls
    updated codereader

--- IDE/Compiler/cParser.js
+++ IDE/Compiler/cParser.js
@@ -3,12 +3,12 @@
 //codeLine = index of line where start of c-tag was found
 //return = string with all the lines of c found or error
 function cParser(codeLines, codeLine){
-    let cCode = "//your c\n";
+    let cCode = "//Unparsed C code\n";
     for (let i = codeLine + 1; i < codeLines.length; i++){
-        if (codeLines[i] === "}"){
+        if (codeLines[i].replace(/\s/g, "") === "#"){
             return [cCode, i];
         };
         cCode += codeLines[i] + "\n";
     };
-    return [{"id": "error", "type": "error", "error": "c-code missing }"}, i];
+    return [{"id": "error", "type": "error", "error": "c-code missing #"}, i];
 };
